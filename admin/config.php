1592f0cb19e80a482d8450467cb706be736082f2
Christian Hizon <christiangerardhizon@gmail.com>
2021-06-11 15:08:25 +0800

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://staging.mushu.ph/admin/');
+define('HTTP_CATALOG', 'http://staging.mushu.ph/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://staging.mushu.ph/admin/');
+define('HTTPS_CATALOG', 'https://staging.mushu.ph/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/staging_mushu/admin/');
+define('DIR_SYSTEM', '/var/www/staging_mushu/system/');
+define('DIR_IMAGE', '/var/www/staging_mushu/image/');
+define('DIR_STORAGE', '/var/www/staging_mushu/system/storage/');
+define('DIR_CATALOG', '/var/www/staging_mushu/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'staging_mushu_opencart');
+define('DB_PASSWORD', 'bmQqE)e84wW*p&(m');
+define('DB_DATABASE', 'staging_mushu_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://staging.mushu.ph/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://staging.mushu.ph/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/staging_mushu/catalog/');
+define('DIR_SYSTEM', '/var/www/staging_mushu/system/');
+define('DIR_IMAGE', '/var/www/staging_mushu/image/');
+define('DIR_STORAGE', '/var/www/staging_mushu/system/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'staging_mushu_opencart');
+define('DB_PASSWORD', 'bmQqE)e84wW*p&(m');
+define('DB_DATABASE', 'staging_mushu_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
